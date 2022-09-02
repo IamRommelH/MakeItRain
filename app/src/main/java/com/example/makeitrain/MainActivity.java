9e2415c967e8cc66e2e232b9fb6a6c164d30593b
Rommel Hipos <Rommel.Hipos@wdc.com>
2022-09-02 10:24:14 +0800

    init

--- app/src/main/java/com/example/makeitrain/MainActivity.java
+++ app/src/main/java/com/example/makeitrain/MainActivity.java
@@ -26,39 +26,5 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        makeItRain = findViewById(R.id.buttonMakeItRain);
-        moneyValue = findViewById(R.id.moneyValue);
-
-//        makeItRain.setOnClickListener(new View.OnClickListener() {
-//            @Override
-//            public void onClick(View v) {
-//                NumberFormat numberFormat = NumberFormat.getCurrencyInstance();
-//                moneyCounter += 1000;
-//                moneyValue.setText(String.valueOf(numberFormat.format(moneyCounter)));
-//                Log.d("MIR", "onClick: " + moneyCounter);
-//            }
-//        });
-    }
-
-    public void makeItRain(View view) {
-        NumberFormat numberFormat = NumberFormat.getCurrencyInstance();
-        moneyCounter += 2000;
-        moneyValue.setText(String.valueOf(numberFormat.format(moneyCounter)));
-        if (moneyCounter > 20000){
-            moneyValue.setTextColor(Color.RED);
-        }
-    }
-
-    public void showInfo(View view) {
-//        Toast.makeText(MainActivity.this, R.string.app_info, Toast.LENGTH_SHORT)
-//            .show();
-        Snackbar.make(moneyValue, R.string.app_info, Snackbar.LENGTH_LONG)
-                .setAction("More", new View.OnClickListener() {
-                    @Override
-                    public void onClick(View v) {
-                        Log.d("Snack", "showInfo: Hello!");
-                    }
-                })
-                .show();
     }
 }
